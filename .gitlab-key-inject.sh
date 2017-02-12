0ea65f1a29df8308a962ccf1fcd9dea0fb769574
Huiren Woo <giantcrabby@gmail.com>
2017-02-12 21:02:52 +0000

    Fixed SSH key weird issues thanks to @gandra in issue #1

--- .gitlab-key-inject.sh
+++ .gitlab-key-inject.sh
@@ -14,6 +14,8 @@ fi
 eval $(ssh-agent -s)
 
 ssh-add <(echo "$SSH_PRIVATE_KEY")
+echo "$SSH_PRIVATE_KEY" > ~/.ssh/staging_deploy_private_key
+chmod 400 ~/.ssh/staging_deploy_private_key
 
 mkdir -p ~/.ssh
 [[ -f /.dockerenv ]] && echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
