1364f03a0fcf4f6a817d28aeeeb99c2e8c608fc1
Huiren Woo <giantcrabby@gmail.com>
2017-02-13 00:35:28 +0000

    Added use of dependecies and artifacts

--- .gitlab-test.sh
+++ .gitlab-test.sh
@@ -2,5 +2,11 @@
 # Ensure we fail fast if there is a problem.
 set -eo pipefail
 
+# Ensure that php is working
+php -v
+
+# Ensure that mysql server is up and running
+ping -c 3 mysql
+
 # Run unit testing with PHPUnit
 php vendor/bin/phpunit --colors
